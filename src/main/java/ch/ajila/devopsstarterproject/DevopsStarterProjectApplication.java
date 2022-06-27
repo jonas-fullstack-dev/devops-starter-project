77b5a9f9411ceaf2827ee63c9929173274d56268
jonas-fullstack-dev <jonas.furrer@hotmail.com>
2022-06-27 11:19:42 +0200

    Init project

---
+++ src/main/java/ch/ajila/devopsstarterproject/DevopsStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package ch.ajila.devopsstarterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStarterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStarterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ajila/devopsstarterproject/DevopsStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ajila.devopsstarterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
